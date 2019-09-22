f3fdfa333869ce526ed3043af2d04a206b08f976
SheekhaJ <jariwals@oregonstate.edu>
2019-09-22 16:46:31 -0700

    Added nodemon as dev dependency and exported users and skills

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-var neo = require('neo4j-driver').v1
+var neo = require('neo4j-driver').v1;
 
-const url = 'bolt://localhost'
+const url = 'bolt://localhost';
 
 var driver = neo.driver(url, neo.auth.basic("neo4j", "password"), {maxTransactionRetryTime: 30000});
 var session = driver.session()
 
-session.readTransaction(function(transaction){
+var getUsers = () => {session.readTransaction(function(transaction){
     var result = transaction.run('match (u:user) return (u)');
     return result
 }).then(function(result){
@@ -13,6 +13,19 @@ session.readTransaction(function(transaction){
     result.records.map(record => console.log(record.get('u')))
 }).catch(function(error){
     console.log('error: '+error)
-})
+})};
+
+var getSkills = () => {session.readTransaction(function(transaction){
+    var result = transaction.run('match(u:user)-[kRel:knows]->(l:language) return u,l');
+    return result
+}).then(function(result){
+    session.close()
+    result.records.forEach((record)=>console.log(`user "${record.get('u')}" knows "${record.get('l')}"`));
+}).catch(function(error){
+    console.log('error: '+error)
+})};
 
 driver.close()
+
+module.exports.getUsers = getUsers;
+module.exports.getSkills = getSkills;
