2292227fe4c8cf258056e02067004a75066567d7
SheekhaJ <jariwals@oregonstate.edu>
2019-09-27 01:30:47 -0700

    Code to fetch user details using axios in useEffect hook

--- client/src/components/Results/UserGrid.js
+++ client/src/components/Results/UserGrid.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Result from './Result'
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
@@ -17,43 +17,39 @@ const useStyles = makeStyles(theme => ({
 
 const baseUrl = 'http://localhost:3001';
 
-function getUserInfo(){
-    axios.get(baseUrl+'/users')
-    .then((response)=>{
-        results = response.data.result.records;
-        // result.map((r)=>console.log(r._fields))
-        results.map((r)=>{
-            const firstName = r._fields[0]
-            const lastName = r._fields[1]
-            const email = r._fields[2]
-            const githubUrl = r._fields[3]
-            const linkedinUrl = r._fields[4]
-            // console.log(firstName, lastName, email, githubUrl, linkedinUrl);
-        });
-    }).catch((error)=>{
-        console.log('error: '+error);
-    });
-}
-
 function UserGrid() {
-    const [spacing, setSpacing] = React.useState(2);
+    const [spacing, setSpacing] = useState(2);
     const classes = useStyles();
+    const [results, setResults] = useState([]);
 
-    // const results = [
-    //     { id: 1, firstName: 'Sheekha', lastName: 'Jariwala', skills: 'Python, Javascript' },
-    //     { id: 2, firstName: 'Vijay', lastName: 'Tadimeti', skills: 'Python, Ruby' },
-    //     { id: 3, firstName: 'Sonam', lastName: 'Gyanchandani', skills: 'Python, HTML' },
-    //     { id: 4, firstName: 'Damanpreet', lastName: 'Kaur', skills: 'Python' }]
+    useEffect(()=>{
+        axios.get(baseUrl+'/users')
+        .then((response)=>{
+            var res = response.data.result.records;
+            var results = new Array();
 
-    // const resultList = results.map(res => <Result key={res.id} result={res}></Result>)
-    const results = getUserInfo();
+            for(var i=0; i<res.length; i++){
+                var r = res[i]
+                const id = r._fields[0]
+                const firstName = r._fields[1]
+                const lastName = r._fields[2]
+                const email = r._fields[3]
+                const githubUrl = r._fields[4]
+                const linkedinUrl = r._fields[5]
+                results.push([id, firstName, lastName, email, githubUrl, linkedinUrl]);
+            }
+            setResults(results)
+        }).catch((error)=>{
+            console.log('error: '+error);
+        });
+    }, [])
+
     return (
-        <Grid container className={classes.root} spacing={2}>
+        <Grid container className={classes.root} spacing={10}>
             <Grid item xs={12}>
                 <Grid container justify="center" spacing={spacing}>
                     {results.map(result => (
                         <Grid key={result.id} item>
-                            {/* <Paper className={classes.paper} >{result.firstName}</Paper> */}
                             <Result key={result.id} result={result}></Result>
                         </Grid>
                     ))}
