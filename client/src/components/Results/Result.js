f33f8fe67ad05ad3d8b9709347c828dc2c963a26
SheekhaJ <jariwals@oregonstate.edu>
2019-09-27 01:30:18 -0700

    Added popover elements and keys for every child

--- client/src/components/Results/Result.js
+++ client/src/components/Results/Result.js
@@ -7,30 +7,84 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Popover from '@material-ui/core/Popover';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     card: {
         maxWidth: 345,
     },
-});
+    typography: {
+        padding: theme.spacing(2),
+    },
+}));
 
 function Result({result}) {
     const classes = useStyles();
-    const {firstName, lastName, skills} = result
+    const [id, firstName, lastName, email, githubUrl, linkedinUrl] = result
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const popoverid = open ? 'simple-popover' : undefined;
 
     return (
-        <Card className={classes.card}>
-            <CardActionArea>
-                <CardMedia component="img" alt={firstName} height="140" image="/static/images/cards/contemplative-reptile.jpg" title="Contemplative Reptile" />
+        <Card className={classes.card} key={result.id}>
+            <CardActionArea key={result.id}>
+                <CardMedia component="img" key={result.id} alt={firstName} height="140" image="/static/images/cards/contemplative-reptile.jpg" title="Contemplative Reptile" />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2"> {firstName}+{lastName} </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p"> Skills: {skills} </Typography>
+                    <Typography gutterBottom variant="h5" key={result.id} component="h2"> {firstName} {lastName} </Typography>
+                    <Typography variant="body2" color="textSecondary" key={result.id} component="p"> Skills: {email} </Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary"> Email </Button>
-                <Button size="small" color="primary"> GitHub </Button>
-                <Button size="small" color="primary"> LinkedIn </Button>
+            <CardActions key={result.id}>
+                <Button size="small" key={result.id} color="primary" variant="contained" onClick={handleClick}> Email </Button>
+                <Popover
+                id={popoverid} open={open} anchorEl={anchorEl} onClose={handleClose}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'center',
+                }}>
+                    <Typography key={result.id} className={classes.typography}>{email}</Typography>
+                </Popover>
+                
+                <Button size="small" color="primary" variant="contained" onClick={handleClick}> GitHub </Button>
+                <Popover
+                id={popoverid} open={open} anchorEl={anchorEl} onClose={handleClose}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'center',
+                }}>
+                    <Typography key={result.id} className={classes.typography}>{githubUrl}</Typography>
+                </Popover>
+
+                <Button size="small" color="primary" variant="contained" onClick={handleClick}> Linkedin </Button>
+                <Popover
+                id={popoverid} open={open} anchorEl={anchorEl} onClose={handleClose}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'center',
+                }}>
+                    <Typography key={result.id} className={classes.typography}>{linkedinUrl}</Typography>
+                </Popover>
             </CardActions>
         </Card>
     )
