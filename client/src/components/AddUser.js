acf4ad480146556defd2e757c4f31d30c8d35a45
SheekhaJ <jariwals@oregonstate.edu>
2019-10-02 23:28:31 -0700

    Added user state object

--- client/src/components/AddUser.js
+++ client/src/components/AddUser.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -7,18 +7,26 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001';
 
 export default function AddUser() {
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [user, setUser] = useState({firstName: '', lastName: '', email: '', githubUrl: '', linkedinUrl: ''})
 
     const handleClickOpen = () => {
-        console.log('handle click open called!')
       setOpen(true);
     };
 
     const handleClose = () => {
       setOpen(false);
-    };
+  };
+  
+  const handleSubmit = () => {
+    var bodyFormData = user;
+    console.log('form data: ',bodyFormData)
+  }
 
     return (
       <div>
@@ -41,47 +49,65 @@ export default function AddUser() {
         >
           <DialogTitle id="form-dialog-title">Add User</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              Enter user details:
-            </DialogContentText>
+            <DialogContentText> Enter user details: </DialogContentText>
             <TextField
               autoFocus
               margin="dense"
               id="firstName"
               label="First Name"
+              value={user.firstName}
               fullWidth
+              onChange={e => {
+                setUser({ ...user, firstName: e.target.value });
+              }}
             />
             <TextField
               margin="dense"
               id="lastName"
               label="Last Name"
+              value={user.lastName}
               fullWidth
+              onChange={e => {
+                setUser({ ...user, lastName: e.target.value });
+              }}
             />
             <TextField
               margin="dense"
               id="email"
               label="Email Address"
               type="email"
+              value={user.email}
               fullWidth
+              onChange={e => {
+                setUser({ ...user, email: e.target.value });
+              }}
             />
             <TextField
               margin="dense"
               id="githubUrl"
               label="GitHub URL"
+              value={user.githubUrl}
               fullWidth
+              onChange={e => {
+                setUser({ ...user, githubUrl: e.target.value });
+              }}
             />
             <TextField
               margin="dense"
               id="linkedinUrl"
               label="LinkedIn URL"
+              value={user.linkedinUrl}
               fullWidth
+              onChange={e => {
+                setUser({ ...user, linkedinUrl: e.target.value });
+              }}
             />
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose} color="primary">
               Cancel
             </Button>
-            <Button onClick={handleClose} color="primary">
+            <Button onClick={handleSubmit} color="primary">
               Submit
             </Button>
           </DialogActions>
