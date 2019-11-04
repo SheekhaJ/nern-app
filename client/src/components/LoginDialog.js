58908f3b223bfb80237eac1ec090419551b71d77
SheekhaJ <jariwals@oregonstate.edu>
2019-11-04 10:40:56 -0800

    Code to get email of user from login dialog

--- client/src/components/LoginDialog.js
+++ client/src/components/LoginDialog.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
@@ -6,9 +6,12 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import axios from 'axios';
 
 function LoginDialog() {
-    const [open, setOpen] = React.useState(false);
+    const baseUrl = "http://localhost:3001";
+    const [open, setOpen] = useState(false);
+    const [username, setUsername] = useState('');
 
     const handleClickOpen = () => {
       setOpen(true);
@@ -16,6 +19,10 @@ function LoginDialog() {
 
     const handleClose = () => {
       setOpen(false);
+    };
+
+    const login = (username) => {
+        console.log('login clicked ', username);
     };
 
     return (
@@ -30,23 +37,23 @@ function LoginDialog() {
         >
           <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              Log In
-            </DialogContentText>
+            <DialogContentText>Log In</DialogContentText>
             <TextField
               autoFocus
               margin="dense"
               id="name"
               label="Email Address"
               type="email"
+              value={username}
               fullWidth
+                onChange={e => {setUsername(e.target.value);}}
             />
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose} color="primary">
               Cancel
             </Button>
-            <Button onClick={handleClose} color="primary">
+                    <Button onClick={() => {login(username)}} color="primary">
               Log In
             </Button>
           </DialogActions>
