c55216d31a79338170cd7dca04233446a73b69b7
SheekhaJ <jariwals@oregonstate.edu>
2019-11-07 23:29:05 -0800

    Added main parent surface to contain other child surfaces

---
+++ client/src/components/Layouts/MainSurface.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from "./index";
+import ResultSurface from "./ResultSurface";
+
+
+function MainSurface(props) {
+    return (
+      <div>
+        <Header />
+        <ResultSurface />
+      </div>
+    );
+}
+
+export default MainSurface
