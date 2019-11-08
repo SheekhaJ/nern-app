19739e6dfe3dfe302d4433de641497d822d94583
SheekhaJ <jariwals@oregonstate.edu>
2019-11-07 23:49:16 -0800

    Using MainSurface.js instead of Surface.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Header} from './components/Layouts/index'
-import Surface from './components/Layouts/Surface';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
+import MainSurface from './components/Layouts/MainSurface';
 
 const store = configureStore()
 
 function App() {
   console.log('store is ' , store.getState())
-  // debugger;
   return (
     <Provider store={store}>
     <div className="App">
-      <Header />
-      <Surface/>
-      {/* <Footer/> */}
+      <MainSurface/>
     </div>
     </Provider>
   );
