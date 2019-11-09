48513e8d638c8be501cd881e80af0da714aaba3d
SheekhaJ <jariwals@oregonstate.edu>
2019-11-08 23:00:15 -0800

    Added container for the app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
+import Container from "@material-ui/core/Container";
 import MainSurface from './components/Layouts/MainSurface';
 
 const store = configureStore()
@@ -10,9 +11,11 @@ function App() {
   console.log('store is ' , store.getState())
   return (
     <Provider store={store}>
-    <div className="App">
-      <MainSurface/>
-    </div>
+      <Container maxWidth='xl'>
+        <div className="App">
+          <MainSurface />
+        </div>
+      </Container>
     </Provider>
   );
 }
