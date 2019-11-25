fd439fba912046893883bee94c96bb80b14cbaa5
SheekhaJ <jariwals@oregonstate.edu>
2019-11-25 14:17:38 -0800

    Conditionally rendering ProfileSurface and ResultSurface in MainSurface component

--- client/src/components/Layouts/MainSurface.js
+++ client/src/components/Layouts/MainSurface.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Header } from "./index";
 import ResultSurface from "./ResultSurface";
+import ProfileSurface from "./ProfileSurface";
 
 function MainSurface(props) {
+  const [profileUserid, setProfileUserid] = useState();
+  
+  const getUseridForProfile = (userid) => {
+    console.log('In mainsurface userid: ', userid);
+    setProfileUserid((userid) => {console.log('inside setstate: ',userid,profileUserid)});
+    console.log('profileuserid in main surface: ', profileUserid);
+    };
+      
     return (
       <div>
-        <Header/>
-        <ResultSurface />
+        <Header />
+        {!profileUserid && <ResultSurface profileid={getUseridForProfile} />}
+        {/* { <ProfileSurface profileid={17} />} */}
+        {profileUserid && <ProfileSurface profileid={profileUserid} />}
       </div>
     );
 }
