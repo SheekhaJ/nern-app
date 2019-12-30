4b771f63a16c8b946c60c8f7edc294c61025e16e
Sheekha Jariwala <jariwals@oregonstate.edu>
2019-12-29 17:12:58 -0800

    Added constraints.js file to store constraints for validation of form fields

---
+++ client/src/Util/constraints.js
@@ -0,0 +1,36 @@
+const constraints = {
+    firstName: {
+        presence: {
+            allowEmpty: false
+        },
+        type: 'string'
+    },
+    lastName: {
+        presence: {
+            allowEmpty: false
+        },
+        type: 'string'
+    },
+    emailAddress: {
+        email: {
+            message: '^Email address is invalid'
+        },
+        presence: {
+            allowEmpty: false
+        },
+        type: 'string'
+    },
+    githubUrl: {
+        url: {
+            allowLocal: false
+        }
+    },
+    linkedinUrl: {
+        url: {
+            allowLocal: false
+        }
+    }
+
+}
+
+export default constraints;
