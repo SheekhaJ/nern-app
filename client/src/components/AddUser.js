179ae3a1bb39c30ee233cd3a39ec7baa07e8d532
Sheekha Jariwala <jariwals@oregonstate.edu>
2019-12-31 00:12:27 -0800

    Removed comments and custom dialog alert on error

--- client/src/components/AddUser.js
+++ client/src/components/AddUser.js
@@ -16,7 +16,6 @@ export function AddUser(props) {
 
   const [open, setOpen] = useState(false);
   const [user, setUser] = useState({ firstName: '', lastName: '', email: '', githubUrl: '', linkedinUrl: '' })
-  const [openAlert, setOpenAlert] = useState(false);
   const [errors, setErrors] = useState(null);
 
     const handleClickOpen = () => {
@@ -30,22 +29,14 @@ export function AddUser(props) {
   const handleSubmit = () => {
     const errs = validate({ firstName: user.firstName, lastName: user.lastName, emailAddress: user.email, githubUrl: user.githubUrl, linkedinUrl: user.linkedinUrl },
     {firstName: constraints.firstName, lastName: constraints.lastName, emailAddress: constraints.emailAddress, githubUrl: constraints.githubUrl, linkedinUrl: constraints.linkedinUrl});
-    console.log('errs: ', errs);
-
+    
     if (errs) {
       setErrors(errs)
-    // }
-    // if (user.firstName === '' || user.lastName === '') {
-    //   setOpenAlert(true)
     } else {
       props.addNewUser(user);
       setOpen(false);
       setUser({ firstName: '', lastName: '', email: '', githubUrl: '', linkedinUrl: '' });
     }
-  }
-
-  const handleAlertClose = () => {
-    setOpenAlert(false);
   }
 
     return (
@@ -145,15 +136,6 @@ export function AddUser(props) {
             </Button>
           </DialogActions>
         </Dialog>
-
-        {/* <Dialog aria-labelledby='alert-dialog-title' aria-describedby='alert-dialog-description' open={openAlert} onClose={handleAlertClose}>
-          <DialogTitle id='alert-dialog-title'>{"Invalid input for new user"}</DialogTitle>
-          <DialogContent id='alert-dialog-description'>First and Last name of new user are mandatory</DialogContent>
-          <DialogActions>
-            <Button onClick={handleAlertClose} color='primary'>Cancel</Button>
-            <Button onClick={handleAlertClose} color='primary'>Ok</Button>
-          </DialogActions>
-        </Dialog> */}
       </div>
     );
 }
