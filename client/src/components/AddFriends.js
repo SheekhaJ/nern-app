632f094b3af8d9806b4758d2256e93a338bdeefb
Sheekha Jariwala <jariwals@oregonstate.edu>
2020-01-28 20:40:47 -0800

    Code to display the 'Add Friends' popup only when the user has been authenticated successfully.

--- client/src/components/AddFriends.js
+++ client/src/components/AddFriends.js
@@ -1,15 +1,25 @@
 import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 function AddFriends(props) {
+    const [displayAlert, setDisplayAlert] = useState(null);
 
     const handleClickOpen = (e) => {
         console.log('add friend button clicked! type ', localStorage.getItem('eruid') === 'undefined');
         if (localStorage.getItem('eruid') =='undefined' && localStorage.getItem('erAuthFirstName') == 'undefined' && localStorage.getItem('erAuthLastName') == 'undefined') {
-            alert('you need to be logged in to add friends!');
+            setDisplayAlert(true);
         } else {
-            alert('you are logged in and can add friends!');
+            setDisplayAlert(false);
         }
+    }
+
+    const handleAddFriendDialogClose = () => {
+        setDisplayAlert(false);
     }
 
     return (
@@ -17,6 +27,20 @@ function AddFriends(props) {
             <Button variant="contained" color="primary" onClick={handleClickOpen}>
                 Add Friends
             </Button>
+            <Dialog
+                open={displayAlert}
+                onClose={handleAddFriendDialogClose}
+                aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Add Friends</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>You must be logged in to add friends!</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleAddFriendDialogClose} color="primary">
+                        Ok
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
