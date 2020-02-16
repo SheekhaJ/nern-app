323ed0b950113f1813377877eda345e6fc680201
Sheekha Jariwala <jariwals@oregonstate.edu>
2020-02-15 22:41:32 -0800

    Code to display alert if user tries to submit rating for a skill without logging in

--- client/src/components/SkillRating.js
+++ client/src/components/SkillRating.js
@@ -1,17 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import Rating from '@material-ui/lab/Rating';
 import Button from "@material-ui/core/Button";
-import { connect } from 'react-redux';
-import { getUserRatings } from '../redux/actions';
-
-const handleLanguageClick = (e) => {
-    console.log('event triggered by ', e.currentTarget);
-}
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 function SkillRating(props) {
     const [value, setValue] = useState(props.rating);
+    const [displayAlert, setDisplayAlert] = useState(false);
     const [profileuserid, setProfileuserid] = useState(props.selectedprofileuserid);
     // const [userRatings, setUserRatings] = useState(props.ratings);
+
+
+    const handleLanguageClick = (e) => {
+        // console.log('event triggered by ', e.currentTarget);
+        if (localStorage.getItem('eruid') == 'undefined' && localStorage.getItem('erAuthFirstName') == 'undefined' && localStorage.getItem('erAuthLastName') == 'undefined') {
+            setDisplayAlert(true);
+        } else {
+            setDisplayAlert(false);
+        }
+    }
+
+    const handleAlertClose = () => {
+        setDisplayAlert(false);
+    }
 
     useEffect(() => {
         console.log('loggedinuserid - ', props);
@@ -25,6 +39,20 @@ function SkillRating(props) {
     // console.log('props in skill rating component - ', props);
     return (
         <div>
+            <Dialog
+                open={displayAlert}
+                onClose={handleAlertClose}
+                aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Submit Rating</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>You must be logged in to submit rating for user's skill!</DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleAlertClose} color="primary">
+                        Ok
+                    </Button>
+                </DialogActions>
+            </Dialog>
             <Button color="primary" onClick={(e) => handleLanguageClick(e.target.innerHTML)} >{props.language}</Button>
             <Rating name="half-rating" value={value} onChange={(e,newValue) => (console.log('newval: ',newValue), setValue(newValue))} />
         </div>
